adbcd6aa55d4ea2d88a86028cfdc1825bd183dab
Kam Gebski <1512307@stu.ucenmanchester.ac.uk>
2024-11-08 14:17:38 +0000

    listops.py

---
+++ listops.py
@@ -0,0 +1,38 @@
+integer_list = [51, 50, 21, 5, 12, 7, 15, 9, 10, 70, 770, ]
+
+def print_the_list(lis):
+    print(*lis, sep=', ')
+    
+def sum_list(lis):
+    result = 0
+    for item in lis:
+        result+=item
+    print(f'sum of elements in the {lis} is {result}')
+def print_largest(lis):
+    print(max(integer_list))
+
+def reverted_list(lis):
+    print(lis[::-1])
+
+def smallest_in_list(lis):
+    result = lis[0]
+    for item in lis:
+        if item <= result:
+            result = item
+    print(result)
+def smallest_fucional(lis):
+    print(min(lis))
+        
+        
+      
+print_the_list(integer_list)
+
+sum_list(integer_list)
+
+print_largest(integer_list)
+
+reverted_list(integer_list)
+
+smallest_in_list(integer_list)
+
+smallest_fucional(integer_list)
