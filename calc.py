9bce4f92c9588baecdf7d6240edbf5cf5cb5f34d
Kam Gebski <1512307@stu.ucenmanchester.ac.uk>
2024-11-08 13:27:30 +0000

    calc.py assessment 1

---
+++ calc.py
@@ -0,0 +1,12 @@
+first_num = int(input('Type first number '))
+second_num = int(input('Type second number '))
+
+
+output = f'''
+{first_num} + {second_num} =  {(first_num)+second_num}
+{first_num} - {second_num} =  {(first_num)-second_num}
+{first_num} * {second_num} =  {(first_num)*second_num}
+{first_num} / {second_num} =  {(first_num)/second_num}
+
+'''
+print(output)
