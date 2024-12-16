5d449ff914f2eb31ed6a036585ff4987f83cfdff
GeKamB <k.gebski1@gmail.com>
2024-12-16 18:15:57 +0000

    Improve temp formula

--- functions.py
+++ functions.py
@@ -1,14 +1,14 @@
 def temp_converter():
-    conversion_type = input("Choose convertion type 1 for celsius to fahrenheit, type 2 for fahrenheit to celsius ")
+    conversion_type = input("Choose convertion type 1 for fahrenheit to celsius, type 2 for celsius to fahrenheit")
     sign = ""
     temp = int(input('temperature value '))
     if conversion_type == '1':
         sign = "°C"
-        temp = (temp * (9/5)) +32
+        temp = (temp - 32) / (9/5)
         print(temp, sign)
     elif conversion_type == '2':
         sign = "°F"
-        temp = (temp - 32) * (5/9)
+        temp = (temp * (9/5))+32
         print(temp, sign)
     else:
         print("You typed something wrong!")
